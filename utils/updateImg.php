629ddea3b6c69ca69c829cfa979a479e12bd23cf
danielcasta0398 <98791147+danielcasta0398@users.noreply.github.com>
2022-07-08 09:31:38 +0200

    img

---
+++ utils/updateImg.php
@@ -0,0 +1,108 @@
+<?php
+  
+  session_start();
+  $id = session_id();
+  
+
+  function uploadImgUser ($fileArray, $idSession, $task){
+
+    if (isset($_POST['dataImg'])) {
+        $file = $_FILES['img']['name'];   
+        $typeFile = explode(".", $file)[1];
+        $message = '';
+    
+        if (isset($file) && $file != "") {
+            //Obtenemos algunos datos necesarios sobre el archivo
+            $tipo = $_FILES['img']['type'];
+            $tamano = $_FILES['img']['size'];
+            $temp = $_FILES['img']['tmp_name'];
+            //Se comprueba si el archivo a cargar es correcto observando su extensión y tamaño
+           if (!((strpos($tipo, "gif") || strpos($tipo, "jpeg") || strpos($tipo, "jpg") || strpos($tipo, "png")))) {
+              echo '<div><b>Error. La extensión no se permite<br/>
+              - Se permiten archivos .gif, .jpg, .png.</b></div>';
+           }
+           else{             
+
+            if ($task == "register") {
+
+                $pathFile = file_exists("../imgUsers/$idSession");
+                $pathFileProfile = file_exists("../imgUsers/$idSession/profile");
+
+                if (!$pathFile) {
+                    mkdir("../imgUsers/$idSession");    
+                    mkdir("../imgUsers/$idSession/profile");                
+                } 
+
+                if (!$pathFileProfile && $pathFile) {                    
+                    mkdir("../imgUsers/$idSession/profile");
+                } 
+    
+               if (move_uploaded_file($temp, "../imgUsers/$idSession/profile/profile.$typeFile")) {
+                        
+                    return print_r('Se ha subido correctamente la imagen');
+                    
+                }   
+
+            } elseif ($task == "post") {     
+
+                $pathFile = file_exists("../imgUsers/$idSession/posts");
+                
+                
+
+                if (!$pathFile) {
+                    mkdir("../imgUsers/$idSession/posts");   
+                             
+                } 
+
+                
+               if (move_uploaded_file($temp, "../imgUsers/$idSession/posts/$file")) {                      
+                
+                return print_r("C:/xampp/htdocs/miniredsocial/imgUsers/$idSession/posts/$file");
+                
+            }
+
+               
+
+               /* if (!$pathFileProfile && $pathFile) {                    
+                    mkdir("../imgUsers/$idSession/profile");
+                } 
+    
+               if (move_uploaded_file($temp, "../imgUsers/$idSession/profile/profile.$typeFile")) {
+                        
+                    return print_r('Se ha subido correctamente la imagen');
+                    
+                }         */ 
+            }
+           
+           }
+           
+    
+        }
+    }
+
+  };
+   
+  if (isset($_FILES['img'])) {
+    uploadImgUser($_FILES['img'], $id, "post");
+  }
+
+   
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form method="POST" enctype="multipart/form-data">
+        <input name="img" type="file">
+        <button name="dataImg" >Enviar</button>
+    </form>
+</body>
+</html>
