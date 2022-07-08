45d63c16577eec315b7269c160c83c07f3b71ef5
adrian-af <adrian.afo@hotmail.com>
2022-07-08 12:19:56 +0200

    función para validar el inicio de sesión

--- perfil.php
+++ perfil.php
@@ -1,36 +1,61 @@
 <?php
-    session_start();
-    if(isset($_POST))
+    if(isset($_POST['inicio'])) //se ha iniciado sesión
+    {
+        $email = $_POST['email'];
+        $password = $_POST['password'];
+        //comprobar que el usuario exista
+        if(is_file('./json/data.json')) //se coge el contenido del json
+        {
+            $contenidoJson = file_get_contents('./json/data.json');
+            $contenidoJson = json_decode($contenidoJson, true); //lo devuelve en un array asociativo
+        }
+        else //no hay json, así que todavía no hay ningún usuario registrado
+        {
+            echo "usuario no registrado";
+            die; //aquí se podría por ejemplo redirigir a la página de inicio poniendo un mensaje de error o algo
+        }
+        //comprobar que la contraseña coincida
+        for($i=0; $i<count($contenidoJson); $i++)
+        {
+            if($contenidoJson[$i]['email'] == $email)
+            {
+                if($contenidoJson[$i]['password'] == $password) //el usuario y la contraseña están correctas
+                {
+                    session_id($contenidoJson[$i]['id']);//coger el session ID de ese usuario y se lo asigna a la sesión
+                }
+            }
+        }
+        
+    }
+    elseif(isset($_POST['registro'])) //se ha registrado el usuario
     {
         $nombre = $_POST['nombre'];
         $username = $_POST['username'];
         $email = $_POST['email'];
         $fechaNac = $_POST['fechaNac'];
         $password = $_POST['password'];
-    }
-    if(isset($_FILES))
-    {
+
+        $data = [];
+        if(is_file('data.json'))
+        {
+            $contenidoJson = file_get_contents('data.json');
+            $contenidoJson = json_decode($contenidoJson, true);
+            $data = $contenidoJson;
+        }
+        $miArray = (Object) ['id' => session_id(),
+                                'nombre' => $nombre,
+                                'username' => $username,
+                                'email' => $email,
+                                'fechaNac' => $fechaNac,
+                                'password' => $password];
+
+        $data[] = $miArray;
         
-    }
-    $data = [];
-    if(is_file('data.json'))
-    {
-        $contenidoJson = file_get_contents('data.json');
-        $contenidoJson = json_decode($contenidoJson, true);
-        $data = $contenidoJson;
-    }
-    $miArray = (Object) ['id' => session_id(),
-                            'nombre' => $nombre,
-                            'username' => $username,
-                            'email' => $email,
-                            'fechaNac' => $fechaNac,
-                            'password' => $password];
+        $json = json_encode($data);
 
-    $data[] = $miArray;
-    print_r(stripslashes(json_encode($data)));
-    $json = json_encode($data);
-
-    file_put_contents('data.json', $json);
-    echo "<script src='prueba.js'></script>";
+        file_put_contents('data.json', $json);
+    }
+    session_start();
+    
 ?>
 
