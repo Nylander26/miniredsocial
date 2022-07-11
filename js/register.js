0b3eb87437d75d6fb17b11e6ba74750946112dda
GhassanKY <elkhouryghassan712@gmail.com>
2022-07-11 21:32:00 +0200

    Previsualizacion de Imagen y eliminado, Correcion Input File,

--- js/register.js
+++ js/register.js
@@ -7,12 +7,12 @@ const register = async (e) => {
     e.preventDefault()
 
 
-    let name = document.getElementById("name").value 
-    let username = document.getElementById("username").value      
-    let email = document.getElementById("email").value   
+    let name = document.getElementById("name").value
+    let username = document.getElementById("username").value
+    let email = document.getElementById("email").value
     let birthday = document.getElementById("birthday").value
     let password = document.getElementById("password").value
-    let passwordCheck = document.getElementById("passCheck").value      
+    let passwordCheck = document.getElementById("passCheck").value
     let data = {
         name,
         username,
@@ -21,22 +21,22 @@ const register = async (e) => {
         password,
         passwordCheck
     }
-    
-    const getApi = await fetch('../php/createUser.php',{
-        method : 'POST',
+
+    const getApi = await fetch('../php/createUser.php', {
+        method: 'POST',
         body: JSON.stringify(data)
     })
     const resApi = await getApi.json()
     console.log(resApi);
-    
+
 }
 
 
-image.addEventListener ("change", async () => {
+image.addEventListener("change", async () => {
     const dataForm = new FormData(form)
-    
-    const getApi = await fetch('../php/uploadImg.php',{
-        method : 'POST',
+
+    const getApi = await fetch('../php/uploadImg.php', {
+        method: 'POST',
         body: dataForm
     })
 
@@ -44,4 +44,27 @@ image.addEventListener ("change", async () => {
 
     console.log(resApi);
 
-   })
+})
+
+/*Previsualizacion de Imagen Registro*/
+
+document.getElementById('image').onchange = function (e) {
+    var lector = new FileReader();
+    if (lector) {
+        lector.readAsDataURL(e.target.files[0]);
+        lector.onload = function () {
+            let preview = document.getElementById('previewDIV');
+            let divINPUT = document.getElementById('cajaFile')
+            divINPUT.style.display = "none";
+            imagen = document.createElement('img');
+            imagen.src = lector.result;
+            preview.append(imagen);
+        }
+    }
+}
+
+document.getElementById('previewDIV').onclick = function (e){
+    this.remove();
+    let divINPUT = document.getElementById('cajaFile')
+    divINPUT.style.display = "flex";
+};
