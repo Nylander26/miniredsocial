9f49d7a52416e98ee937350d8346f586eb7c8a5d
adrianmo <amoratillariofrio@gmail.com>
2022-07-11 12:39:42 +0200

    funcion registro completa conecatada con API

--- js/register.js
+++ js/register.js
@@ -1,22 +1,26 @@
 const register = async (e) => {
     e.preventDefault()
     let name = document.getElementById("name").value 
-    let lastName = document.getElementById("lastName").value 
-    let userName = document.getElementById("userName").value      
+    let username = document.getElementById("username").value      
     let email = document.getElementById("email").value   
-    let password = document.getElementById("password").value   
+    let birthday = document.getElementById("birthday").value
+    let password = document.getElementById("password").value
+    let passwordCheck = document.getElementById("passCheck").value      
     let data = {
         name,
-        lastName,
-        userName,
+        username,
         email,
-        password
+        birthday,
+        password,
+        passwordCheck
     }
+    
     const getApi = await fetch('../php/createUser.php',{
         method : 'POST',
         body: JSON.stringify(data)
     })
     const resApi = await getApi.json()
     console.log(resApi);
+    
 }
     //console.log(name, lastName, userName, email, password);
